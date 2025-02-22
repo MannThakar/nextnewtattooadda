f5f20c664d1851c0f071fc6cafff6e69a822e24a
MannThakar14 <76089480+MannThakar@users.noreply.github.com>
2025-02-22 15:59:49 +0530

    feat: fix deployement

--- src/app/(artschool)/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ArtSchool = () => {
-  return (
-    <div>I love you baby, You can start code here please make new css file</div>
-  );
-};
-
-export default ArtSchool;
