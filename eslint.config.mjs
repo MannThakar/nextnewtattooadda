39c429cb385d2778c20a9ba5f0d5e68cb13d3485
MannThakar14 <76089480+MannThakar@users.noreply.github.com>
2025-02-22 16:14:42 +0530

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  {
+    languageOptions: {
+      parser: "@typescript-eslint/parser", // Define the parser manually
+      sourceType: "module",
+    },
+  },
+  ...compat.extends("next/core-web-vitals"), // Use FlatCompat only for extends
+];
 
 export default eslintConfig;
